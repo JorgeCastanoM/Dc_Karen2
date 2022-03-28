449b02911ee91ebaf99d21c071096d4596760fce
jorge1180 <80490272+jorge1180@users.noreply.github.com>
2022-03-28 17:07:29 -0500

    Initial commit

---
+++ juguetes_sexuales/juguetes.js
@@ -0,0 +1,672 @@
+document.getElementById("button_h").addEventListener("click",function()
+{
+    var xh = document.getElementById("hide");
+    if(xh.style.display === "none") {
+        xh.style.display = "block";
+    }
+    else {
+        xh.style.display = "none";
+    }
+})
+
+document.getElementById("button_h2").addEventListener("click",function()
+{
+    var xh = document.getElementById("hide2");
+    if(xh.style.display === "none") {
+        xh.style.display = "block";
+    }
+    else {
+        xh.style.display = "none";
+    }
+})
+
+document.getElementById("button_h3").addEventListener("click",function()
+{
+    var xh = document.getElementById("hide3");
+    if(xh.style.display === "none") {
+        xh.style.display = "block";
+    }
+    else {
+        xh.style.display = "none";
+    }
+})
+
+document.getElementById("button_h4").addEventListener("click",function()
+{
+    var xh = document.getElementById("hide4");
+    if(xh.style.display === "none") {
+        xh.style.display = "block";
+    }
+    else {
+        xh.style.display = "none";
+    }
+})
+
+document.getElementById("button_h5").addEventListener("click",function()
+{
+    var xh = document.getElementById("hide5");
+    if(xh.style.display === "none") {
+        xh.style.display = "block";
+    }
+    else {
+        xh.style.display = "none";
+    }
+})
+
+document.getElementById("btn").addEventListener("click",function()
+{
+    var x = document.getElementById("text");
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+})
+
+document.getElementById("btn1").addEventListener("click",function()
+{
+    var x1 = document.getElementById("text1");
+    if(x1.style.display === "none") {
+        x1.style.display = "block";
+    }
+    else {
+        x1.style.display = "none";
+    }
+})
+
+document.getElementById("btn2").addEventListener("click",function()
+{
+    var x2 = document.getElementById("text2");
+    if(x2.style.display === "none") {
+        x2.style.display = "block";
+    }
+    else {
+        x2.style.display = "none";
+    }
+})
+
+document.getElementById("btn3").addEventListener("click",function()
+{
+    var x3 = document.getElementById("text3");
+    if(x3.style.display === "none") {
+        x3.style.display = "block";
+    }
+    else {
+        x3.style.display = "none";
+    }
+})
+
+document.getElementById("btn4").addEventListener("click",function()
+{
+    var x4 = document.getElementById("text4");
+    if(x4.style.display === "none") {
+        x4.style.display = "block";
+    }
+    else {
+        x4.style.display = "none";
+    }
+})
+
+document.getElementById("btn5").addEventListener("click",function()
+{
+    var x5 = document.getElementById("text5");
+    if(x5.style.display === "none") {
+        x5.style.display = "block";
+    }
+    else {
+        x5.style.display = "none";
+    }
+})
+
+document.getElementById("btn6").addEventListener("click",function()
+{
+    var x6 = document.getElementById("text6");
+    if(x6.style.display === "none") {
+        x6.style.display = "block";
+    }
+    else {
+        x6.style.display = "none";
+    }
+})
+
+document.getElementById("btn7").addEventListener("click",function()
+{
+    var x7 = document.getElementById("text7");
+    if(x7.style.display === "none") {
+        x7.style.display = "block";
+    }
+    else {
+        x7.style.display = "none";
+    }
+})
+
+document.getElementById("btn8").addEventListener("click",function()
+{
+    var x8= document.getElementById("text8");
+    if(x8.style.display === "none") {
+        x8.style.display = "block";
+    }
+    else {
+        x8.style.display = "none";
+    }
+})
+
+document.getElementById("btn9").addEventListener("click",function()
+{
+    var x9= document.getElementById("text9");
+    if(x9.style.display === "none") {
+        x9.style.display = "block";
+    }
+    else {
+        x9.style.display = "none";
+    }
+})
+
+document.getElementById("btn10").addEventListener("click",function()
+{
+    var x10= document.getElementById("text10");
+    if(x10.style.display === "none") {
+        x10.style.display = "block";
+    }
+    else {
+        x10.style.display = "none";
+    }
+})
+
+document.getElementById("btn11").addEventListener("click",function()
+{
+    var x11= document.getElementById("text11");
+    if(x11.style.display === "none") {
+        x11.style.display = "block";
+    }
+    else {
+        x11.style.display = "none";
+    }
+})
+
+document.getElementById("btn12").addEventListener("click",function()
+{
+    var x12= document.getElementById("text12");
+    if(x12.style.display === "none") {
+        x12.style.display = "block";
+    }
+    else {
+        x12.style.display = "none";
+    }
+})
+
+document.getElementById("btn13").addEventListener("click",function()
+{
+    var x13= document.getElementById("text13");
+    if(x13.style.display === "none") {
+        x13.style.display = "block";
+    }
+    else {
+        x13.style.display = "none";
+    }
+})
+
+document.getElementById("btn14").addEventListener("click",function()
+{
+    var x14= document.getElementById("text14");
+    if(x14.style.display === "none") {
+        x14.style.display = "block";
+    }
+    else {
+        x14.style.display = "none";
+    }
+})
+
+document.getElementById("btn15").addEventListener("click",function()
+{
+    var x15= document.getElementById("text15");
+    if(x15.style.display === "none") {
+        x15.style.display = "block";
+    }
+    else {
+        x15.style.display = "none";
+    }
+})
+
+document.getElementById("btn16").addEventListener("click",function()
+{
+    var x16= document.getElementById("text16");
+    if(x16.style.display === "none") {
+        x16.style.display = "block";
+    }
+    else {
+        x16.style.display = "none";
+    }
+})
+
+document.getElementById("btn17").addEventListener("click",function()
+{
+    var x17= document.getElementById("text17");
+    if(x17.style.display === "none") {
+        x17.style.display = "block";
+    }
+    else {
+        x17.style.display = "none";
+    }
+})
+
+document.getElementById("btn18").addEventListener("click",function()
+{
+    var x18= document.getElementById("text18");
+    if(x18.style.display === "none") {
+        x18.style.display = "block";
+    }
+    else {
+        x18.style.display = "none";
+    }
+})
+
+document.getElementById("btn19").addEventListener("click",function()
+{
+    var x19= document.getElementById("text19");
+    if(x19.style.display === "none") {
+        x19.style.display = "block";
+    }
+    else {
+        x19.style.display = "none";
+    }
+})
+
+document.getElementById("btn20").addEventListener("click",function()
+{
+    var x20= document.getElementById("text20");
+    if(x20.style.display === "none") {
+        x20.style.display = "block";
+    }
+    else {
+        x20.style.display = "none";
+    }
+})
+
+document.getElementById("btn21").addEventListener("click",function()
+{
+    var x21= document.getElementById("text21");
+    if(x21.style.display === "none") {
+        x21.style.display = "block";
+    }
+    else {
+        x21.style.display = "none";
+    }
+})
+
+document.getElementById("btn22").addEventListener("click",function()
+{
+    var x22= document.getElementById("text22");
+    if(x22.style.display === "none") {
+        x22.style.display = "block";
+    }
+    else {
+        x22.style.display = "none";
+    }
+})
+
+document.getElementById("btn23").addEventListener("click",function()
+{
+    var x23= document.getElementById("text23");
+    if(x23.style.display === "none") {
+        x23.style.display = "block";
+    }
+    else {
+        x23.style.display = "none";
+    }
+})
+
+document.getElementById("btn24").addEventListener("click",function()
+{
+    var x24= document.getElementById("text24");
+    if(x24.style.display === "none") {
+        x24.style.display = "block";
+    }
+    else {
+        x24.style.display = "none";
+    }
+})
+
+document.getElementById("btn25").addEventListener("click",function()
+{
+    var x25= document.getElementById("text25");
+    if(x25.style.display === "none") {
+        x25.style.display = "block";
+    }
+    else {
+        x25.style.display = "none";
+    }
+})
+
+document.getElementById("btn26").addEventListener("click",function()
+{
+    var x26= document.getElementById("text26");
+    if(x26.style.display === "none") {
+        x26.style.display = "block";
+    }
+    else {
+        x26.style.display = "none";
+    }
+})
+
+document.getElementById("btn27").addEventListener("click",function()
+{
+    var x27= document.getElementById("text27");
+    if(x27.style.display === "none") {
+        x27.style.display = "block";
+    }
+    else {
+        x27.style.display = "none";
+    }
+})
+
+document.getElementById("btn28").addEventListener("click",function()
+{
+    var x28= document.getElementById("text28");
+    if(x28.style.display === "none") {
+        x28.style.display = "block";
+    }
+    else {
+        x28.style.display = "none";
+    }
+})
+
+document.getElementById("btn29").addEventListener("click",function()
+{
+    var x29= document.getElementById("text29");
+    if(x29.style.display === "none") {
+        x29.style.display = "block";
+    }
+    else {
+        x29.style.display = "none";
+    }
+})
+
+document.getElementById("btn30").addEventListener("click",function()
+{
+    var x30= document.getElementById("text30");
+    if(x30.style.display === "none") {
+        x30.style.display = "block";
+    }
+    else {
+        x30.style.display = "none";
+    }
+})
+
+document.getElementById("btn31").addEventListener("click",function()
+{
+    var x31= document.getElementById("text31");
+    if(x31.style.display === "none") {
+        x31.style.display = "block";
+    }
+    else {
+        x31.style.display = "none";
+    }
+})
+
+document.getElementById("btn32").addEventListener("click",function()
+{
+    var x32= document.getElementById("text32");
+    if(x32.style.display === "none") {
+        x32.style.display = "block";
+    }
+    else {
+        x32.style.display = "none";
+    }
+})
+
+document.getElementById("btn33").addEventListener("click",function()
+{
+    var x33= document.getElementById("text33");
+    if(x33.style.display === "none") {
+        x33.style.display = "block";
+    }
+    else {
+        x33.style.display = "none";
+    }
+})
+
+document.getElementById("btn34").addEventListener("click",function()
+{
+    var x34= document.getElementById("text30");
+    if(x34.style.display === "none") {
+        x34.style.display = "block";
+    }
+    else {
+        x34.style.display = "none";
+    }
+})
+
+document.getElementById("btn35").addEventListener("click",function()
+{
+    var x35= document.getElementById("text35");
+    if(x35.style.display === "none") {
+        x35.style.display = "block";
+    }
+    else {
+        x35.style.display = "none";
+    }
+})
+
+document.getElementById("btn36").addEventListener("click",function()
+{
+    var x36= document.getElementById("text36");
+    if(x36.style.display === "none") {
+        x36.style.display = "block";
+    }
+    else {
+        x36.style.display = "none";
+    }
+})
+
+document.getElementById("btn37").addEventListener("click",function()
+{
+    var x37= document.getElementById("text37");
+    if(x37.style.display === "none") {
+        x37.style.display = "block";
+    }
+    else {
+        x37.style.display = "none";
+    }
+})
+
+document.getElementById("btn38").addEventListener("click",function()
+{
+    var x38= document.getElementById("text38");
+    if(x38.style.display === "none") {
+        x38.style.display = "block";
+    }
+    else {
+        x38.style.display = "none";
+    }
+})
+
+document.getElementById("btn39").addEventListener("click",function()
+{
+    var x39= document.getElementById("text39");
+    if(x39.style.display === "none") {
+        x39.style.display = "block";
+    }
+    else {
+        x39.style.display = "none";
+    }
+})
+
+document.getElementById("btn40").addEventListener("click",function()
+{
+    var x40= document.getElementById("text40");
+    if(x40.style.display === "none") {
+        x40.style.display = "block";
+    }
+    else {
+        x40.style.display = "none";
+    }
+})
+
+document.getElementById("btn41").addEventListener("click",function()
+{
+    var x41= document.getElementById("text41");
+    if(x41.style.display === "none") {
+        x41.style.display = "block";
+    }
+    else {
+        x41.style.display = "none";
+    }
+})
+
+document.getElementById("btn42").addEventListener("click",function()
+{
+    var x42= document.getElementById("text42");
+    if(x42.style.display === "none") {
+        x42.style.display = "block";
+    }
+    else {
+        x42.style.display = "none";
+    }
+})
+
+document.getElementById("btn43").addEventListener("click",function()
+{
+    var x43= document.getElementById("text43");
+    if(x43.style.display === "none") {
+        x43.style.display = "block";
+    }
+    else {
+        x43.style.display = "none";
+    }
+})
+
+document.getElementById("btn44").addEventListener("click",function()
+{
+    var x44= document.getElementById("text44");
+    if(x44.style.display === "none") {
+        x44.style.display = "block";
+    }
+    else {
+        x44.style.display = "none";
+    }
+})
+
+document.getElementById("btn45").addEventListener("click",function()
+{
+    var x45= document.getElementById("text45");
+    if(x45.style.display === "none") {
+        x45.style.display = "block";
+    }
+    else {
+        x45.style.display = "none";
+    }
+})
+
+document.getElementById("btn46").addEventListener("click",function()
+{
+    var x46= document.getElementById("text46");
+    if(x46.style.display === "none") {
+        x46.style.display = "block";
+    }
+    else {
+        x46.style.display = "none";
+    }
+})
+
+
+
+document.getElementById("btn47").addEventListener("click",function()
+{
+    var x47= document.getElementById("text47");
+    if(x47.style.display === "none") {
+        x47.style.display = "block";
+    }
+    else {
+        x47.style.display = "none";
+    }
+})
+
+document.getElementById("btn48").addEventListener("click",function()
+{
+    var x48= document.getElementById("text48");
+    if(x48.style.display === "none") {
+        x48.style.display = "block";
+    }
+    else {
+        x48.style.display = "none";
+    }
+})
+
+document.getElementById("btn49").addEventListener("click",function()
+{
+    var x49= document.getElementById("text49");
+    if(x49.style.display === "none") {
+        x49.style.display = "block";
+    }
+    else {
+        x49.style.display = "none";
+    }
+})
+
+document.getElementById("btn50").addEventListener("click",function()
+{
+    var x50= document.getElementById("text50");
+    if(x50.style.display === "none") {
+        x50.style.display = "block";
+    }
+    else {
+        x50.style.display = "none";
+    }
+})
+
+document.getElementById("btn51").addEventListener("click",function()
+{
+    var x51= document.getElementById("text51");
+    if(x51.style.display === "none") {
+        x51.style.display = "block";
+    }
+    else {
+        x51.style.display = "none";
+    }
+})
+
+document.getElementById("btn52").addEventListener("click",function()
+{
+    var x52= document.getElementById("text52");
+    if(x52.style.display === "none") {
+        x52.style.display = "block";
+    }
+    else {
+        x52.style.display = "none";
+    }
+})
+
+document.getElementById("btn53").addEventListener("click",function()
+{
+    var x53= document.getElementById("text53");
+    if(x53.style.display === "none") {
+        x53.style.display = "block";
+    }
+    else {
+        x53.style.display = "none";
+    }
+})
+
+document.getElementById("btn54").addEventListener("click",function()
+{
+    var x54= document.getElementById("text54");
+    if(x54.style.display === "none") {
+        x54.style.display = "block";
+    }
+    else {
+        x54.style.display = "none";
+    }
+})
+
+document.getElementById("btn55").addEventListener("click",function()
+{
+    var x55= document.getElementById("text55");
+    if(x55.style.display === "none") {
+        x55.style.display = "block";
+    }
+    else {
+        x55.style.display = "none";
+    }
+})
